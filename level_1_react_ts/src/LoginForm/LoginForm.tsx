e5692962c86a9071c845f41e28f19f7bc706d070
Igor Barbashov <bis200705@gmail.com>
2024-03-25 13:42:01 +0300

    add Login form

---
+++ level_1_react_ts/src/LoginForm/LoginForm.tsx
@@ -0,0 +1,62 @@
+import {useState, FormEvent} from 'react';
+import styles from './LoginForm.module.css';
+
+export enum FORM_MODE {
+    REGISTER = 'register',
+    LOGIN = 'login'
+};
+
+export const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [formMode, setFormMode] = useState(FORM_MODE.LOGIN);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // request to server
+
+        setEmail('');
+        setPassword('');
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.formContainer}>
+                <h1>Авторизация</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.formGroup}>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='Введите email'
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className={styles.formGroup}>
+                        <label htmlFor='password'>Пароль</label>
+                        <input
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='Введите пароль'
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button type='submit' style={{marginRight: 15}}>
+                        Войти
+                    </button>
+                    <button type='submit' onClick={() => setFormMode(FORM_MODE.REGISTER)}>
+                        Регистрация
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
