1815a46986b79be8a3ffb64b711b3a2590ed3cf5
Igor Barbashov <bis200705@gmail.com>
2024-03-27 17:04:44 +0300

    add validation - if we don't have refreshToken

--- level_4_next_ts_exclusive/src/middleware.ts
+++ level_4_next_ts_exclusive/src/middleware.ts
@@ -1,9 +1,19 @@
 import {NextRequest, NextResponse} from "next/server";
 import {EnumTokens} from "./services/auth.service";
+import {ADMIN_PAGES} from "@/config/pages/admin.config";
+import {PUBLIC_PAGES} from "@/config/pages/public.config";
 
 export async function middleware(request: NextRequest, response: NextResponse) {
     const refreshToken = request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value;
+    const accessToken = request.cookies.get(EnumTokens.ACCESS_TOKEN)?.value;
+
+    const isAdminPage = request.url.includes(ADMIN_PAGES.HOME);
     const isProfilePage = request.url.includes('/profile');
+
+    if (!refreshToken) {
+        request.cookies.delete(EnumTokens.ACCESS_TOKEN);
+        redirectToLogin(isAdminPage, request);
+    }
 
     if (isProfilePage && !refreshToken) {
         return NextResponse.redirect(new URL('/login', request.url));
@@ -13,5 +23,11 @@ export async function middleware(request: NextRequest, response: NextResponse) {
 }
 
 const config = {
-    matcher: ['/profile/:path*'],
+    matcher: ['/admin/:path*', '/profile/:path*'],
+};
+
+const redirectToLogin = (isAdminPage: boolean, request :NextRequest) => {
+    return NextResponse.redirect(
+        new URL(isAdminPage ? '/404' : PUBLIC_PAGES.LOGIN, request.url)
+    );
 };
