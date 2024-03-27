b9519fed89166fd8506cba09466e2b2ca6ecb462
Igor Barbashov <bis200705@gmail.com>
2024-03-27 14:26:12 +0300

    add ProfilePage with server request (edge) fetchProfile

--- level_3_next_ts/src/app/profile/page.tsx
+++ level_3_next_ts/src/app/profile/page.tsx
@@ -1,7 +1,33 @@
-export default function ProfilePage() {
+import {cookies} from "next/headers";
+import {EnumTokens} from "@/services/auth.service";
+import {API_URL} from "@/constants";
+
+const fetchProfile = async () => {
+    'use server'
+
+    const cookie = cookies();
+    const accessToken = cookie.get(EnumTokens.ACCESS_TOKEN)?.value;
+
+    return fetch(`${API_URL}/auth/profile`, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    }).then(res => res.json());
+}
+
+export default async function ProfilePage() {
+    const profile = await fetchProfile();
+
     return (
         <div>
-            Profile Page
+            {profile ? (
+                <>
+                    <h1>Profile</h1>
+                    <p>{profile.email}</p>
+                </>
+            ) : (
+                <p>Not found!</p>
+            )}
         </div>
     );
 };
